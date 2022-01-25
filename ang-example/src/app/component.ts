2480612699417fecf1e37b9cc256a0f820404e41
William White <billwhite@RCPCONSULTING.BIZ>
2022-01-25 12:11:16 -0500

    Into p. 254, with my adjustments for TypeScript narrow-casting.

--- ang-example/src/app/component.ts
+++ ang-example/src/app/component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Model } from "src/models/repository.model";
+import { Product } from "src/models/product.model";
 
 @Component({
     selector: "app",
@@ -19,5 +20,32 @@ export class ProductComponent {
             pr = 0;
         }
         return "p-2 " + (pr< 50 ? "bg-info" : "bg-warning");
+    }
+
+    
+    isPriceGTE (productID: number | undefined, price: number | undefined): boolean {
+        let prod: Product | undefined;
+        if(!productID || !price) {
+            return false;
+        } else {
+            prod = this.model.getProduct(productID);
+        }
+
+        let prodPrice: number | undefined;
+        if(!prod){
+            return false;
+        } else{
+            prodPrice = prod.price;
+        }
+
+        if(!prodPrice) {
+            return false;
+        } else {
+            let gte : boolean = true;
+            if(prodPrice < price) {
+                gte = false;
+            }
+            return gte;
+        }
     }
 }
--- ang-example/src/models/repository.model.ts
+++ ang-example/src/models/repository.model.ts
@@ -52,4 +52,5 @@ export class Model {
         }
         return candidate;
     }
+
 }
