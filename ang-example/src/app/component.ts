d7663b4651fbe920efb29b8689824455c2070a52
William White <billwhite@RCPCONSULTING.BIZ>
2022-01-25 19:38:43 -0500

    Through Listing 13-3.

--- ang-example/src/app/component.ts
+++ ang-example/src/app/component.ts
@@ -1,6 +1,6 @@
 import { ApplicationRef, Component } from "@angular/core";
-import { Model } from "src/models/repository.model";
 import { Product } from "src/models/product.model";
+import { Model } from "src/models/repository.model";
 
 @Component({
     selector: "app",
@@ -19,69 +19,23 @@ export class ProductComponent {
         return this.model.getProducts()[position];
     }
 
-    getClassesByPosition(position: number): string {
-        let product = this.getProductByPosition(position);
-        return "p-2 " + (!this.isPriceGTE(product.id, 50) ? "bg-info" : "bg-warning");
-    }
-
-    getClasses(key: number): string {
-        let pr:number;
-        let product = this.model.getProduct(key);
-        
-        if(product?.price) {
-            pr = product.price;
+    getProduct(key: number): Product {
+        let p = this.model.getProduct(key);
+        if(!p) {
+            return new Product();
         } else {
-            pr = 0;
-        }
-        return "p-2 " + (pr< 50 ? "bg-info" : "bg-warning");
-    }
-
-    getStyles(key: number) {
-        let product = this.model.getProduct(key);
-        if(!product) {
-            return null;
-        } else {
-            return {
-                fontSize: "30px",
-                "margin.px": 100,
-                color: this.isPriceGTE(key, 50) ? "red" : "green"
-            }
-        }
-
-    }
-    isPriceGTE (productID: number | undefined, price: number | undefined): boolean {
-        let prod: Product | undefined;
-        if(!productID || !price) {
-            return false;
-        } else {
-            prod = this.model.getProduct(productID);
-        }
-
-        let prodPrice: number | undefined;
-        if(!prod){
-            return false;
-        } else{
-            prodPrice = prod.price;
-        }
-
-        if(!prodPrice) {
-            return false;
-        } else {
-            let gte : boolean = true;
-            if(prodPrice < price) {
-                gte = false;
-            }
-            return gte;
+            return p;
         }
     }
 
-    getClassMap(key: number): Object {
-        return {
-            "text-center bg-danger": this.model.getProduct(key)?.name  == "Kayak",
-            "bg-info": !this.isPriceGTE(key, 50)
-        }
+    getProducts(): Product[] {
+        return this.model.getProducts();
+    }
+    
+    getProductCount(): number {
+        return this.model.getProducts().length;
     }
 
-    fontSizeWithUnits: string = "30px";
-    fontSizeWithoutUnits: string = "30";
+    targetName: string = "Kayak";
+
 }
