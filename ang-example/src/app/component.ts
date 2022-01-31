7a96a5ccf7abbcea0e381edb0958fac88cff41f1
William White <billwhite@RCPCONSULTING.BIZ>
2022-01-31 18:26:33 -0500

    Through Listing 14.6.
    Code includes my adjustments for workiing with TypeScript in strict mode.

--- ang-example/src/app/component.ts
+++ ang-example/src/app/component.ts
@@ -10,19 +10,6 @@ import { Model } from "src/models/repository.model";
 export class ProductComponent {
     model: Model = new Model();
 
-    constructor(ref: ApplicationRef) {
-        (<any>window).appRef = ref;
-        (<any>window).model = this.model;
-    }
-
-    getKey(index: number, product: Product) {
-        return product.id;
-    }
-
-    getProductByPosition(position: number): Product {
-        return this.model.getProducts()[position];
-    }
-
     getProduct(key: number): Product {
         let p = this.model.getProduct(key);
         if(!p) {
@@ -35,11 +22,10 @@ export class ProductComponent {
     getProducts(): Product[] {
         return this.model.getProducts();
     }
-    
-    getProductCount(): number {
-        return this.model.getProducts().length;
+
+    selectedProduct: string | undefined = "None";
+
+    getSelected(product: Product): boolean {
+        return product.name == this.selectedProduct;
     }
-
-    targetName: string = "Kayak";
-
 }
